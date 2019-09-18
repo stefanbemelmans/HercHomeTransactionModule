5e28e96b1e37f05dcb7f77846b0f05f610dfb6d8
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-09-17 23:51:21 -0400

    Front End Components

---
+++ Source/Client/Features/Transactions/Components/TransactionContainer.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionProject.Client.Features.Base.Components;
+
+namespace TransactionProject.Client.Features.Transactions.Components
+{
+    public class TransactionContainerBase : BaseComponent
+    {
+    public int TxId { get; set; }
+    public bool IsClicked { get; set; }
+    public uint Amount { get; set; }
+    public string ToAddress { get; set; }
+
+    public DateTime TxDate { get; set; }
+
+
+    public void OnClicked()
+    {
+
+    }
+
+    }
+}
---
+++ Source/Client/Features/Transactions/Components/TransactionListContainer.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace TransactionProject.Client.Features.Transactions.Components
+{
+  using System;
+  using System.Collections.Generic;
+  using TransactionProject.Api.Features.Transactions;
+  using TransactionProject.Client.Features.Base.Components;
+
+    public class TransactionListContainerBase : BaseComponent
+  {
+    IReadOnlyList<TransactionsDto> ListOfTransactions => TransactionState.ListOfTransactions;
+
+    private bool HasPaid { get; set; } = false;
+    private uint TotalBalance { get; set; }
+  }
+}
