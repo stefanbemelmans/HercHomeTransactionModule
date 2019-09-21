bd58efa9b9a555193837877d00894ed29c4a072f
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-09-21 00:06:09 -0400

    Total Balance Component, UI edits

---
+++ Source/Client/Features/Transactions/Components/TransactionBalanceComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace TransactionProject.Client.Features.Transactions.Components
+{
+  using Microsoft.AspNetCore.Components;
+  using System;
+  using TransactionProject.Client.Features.Base.Components;
+
+  public class TransactionBalanceComponentBase : BaseComponent
+  {
+   
+  }
+}
