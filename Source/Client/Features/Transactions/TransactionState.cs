65366dbc5095429a124f261e9df18b9d18d84088
Steven T. Cramer <Steve.Cramer@TimeWarp.Enterprises>
2019-09-22 06:57:29 +0000

    Make State Immutable using readonly lists

--- Source/Client/Features/Transactions/TransactionState.cs
+++ Source/Client/Features/Transactions/TransactionState.cs
@@ -2,24 +2,25 @@
 {
   using BlazorState;
   using System.Collections.Generic;
+  using System.Linq;
   using TransactionProject.Api.Features.Transactions;
 
   internal partial class TransactionState : State<TransactionState>
   {
-    //private List<TransactionDto> _ListOfTransactions;
+    private List<TransactionDto> _ListOfTransactions;
+    private List<TransactionDto> _TransactionsToPay;
 
-    //public List<TransactionDto> ListOfTransactions => _ListOfTransactions;
-    public List<TransactionDto> ListOfTransactions { get; set; }
+    public IReadOnlyList<TransactionDto> ListOfTransactions => _ListOfTransactions.AsReadOnly();
+    public IReadOnlyList<TransactionDto> TransactionsToPay => _TransactionsToPay.AsReadOnly();
 
-    public int TotalBalance { get; set; }
-    public List<TransactionDto> TransactionsToPay { get; set; }
+    public int TotalBalance => TransactionsToPay.Sum(aTransactionDto => aTransactionDto.TransactionAmount);
 
     public TransactionState()
     {
-      ListOfTransactions = new List<TransactionDto>();
-      TransactionsToPay = new List<TransactionDto>();
+      _ListOfTransactions = new List<TransactionDto>();
+      _TransactionsToPay = new List<TransactionDto>();
     }
 
-    protected override void Initialize() => TotalBalance = 0;
+    protected override void Initialize() { }
   }
 }
