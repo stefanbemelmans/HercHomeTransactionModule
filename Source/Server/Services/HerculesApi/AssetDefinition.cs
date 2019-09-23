02e8d81c55886d282e1437e8a90281fc4cd8bcbe
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-09-23 14:01:43 -0400

    additional types

--- Source/Server/Services/HerculesApi/AssetDefinition.cs
+++ Source/Server/Services/HerculesApi/AssetDefinition.cs
@@ -4,8 +4,24 @@ namespace TransactionProject.Server.Services.HerculesApi
 {
   public class AssetDefinition
   {
+    /// <summary>
+    /// give a name for the asset
+    /// </summary>
     public string AssetName { get; set; }
-    public byte[] Logo { get; set; }
+    /// <summary>
+    /// an image file sent in multipart/form-data, I'm using the base64 string for now
+    /// </summary>
+    /// <summary>
+    public string Base64StringLogo { get; set; }
+
+    /// a JSON object of metrics up to a maximum of eight unique values. 
+    /// Each metric must be labeled as "Metric" plus the number. E.g. {"Metric1": "foo", "Metric2": "bar"}
+    /// </summary>
     public List<Metrics> Metrics { get; set; }
+    /// <summary>
+    /// the username of the allowed person/organization. Must be a current and existing user.
+    /// </summary>
+    public string Permissable { get; set; }
+
   }
 }
