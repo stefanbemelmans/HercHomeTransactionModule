11d613e715c4bd723d5e5d2d14887dffb2cf64a7
Stefan L. Bemelmans <stefanlbemelmans@gmail.com>
2019-09-22 21:03:50 -0400

    providing option to pay all or pay selected transactions

---
+++ Source/Client/Features/Transactions/Actions/Payment/Full/TransactionPaymentFullAction.cs
@@ -0,0 +1,11 @@
+﻿namespace TransactionProject.Client.Features.Transactions
+{
+  using TransactionProject.Client.Features.Base;
+
+  internal partial class TransactionState
+  {
+    public class TransactionPaymentFullAction : BaseAction
+    {
+    }
+  }
+}
---
+++ Source/Client/Features/Transactions/Actions/Payment/Full/TransactionPaymentFullHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace TransactionProject.Client.Features.Transactions
+{
+  using BlazorState;
+  using MediatR;
+  using System.Threading;
+  using System.Threading.Tasks;
+  using TransactionProject.Client.Features.Base;
+  using TransactionProject.Api.Features.Transactions;
+  using System.Collections.Generic;
+
+  internal partial class TransactionState
+  {
+    public class TransactionPaymentHandler : BaseHandler<TransactionPaymentFullAction>
+    {
+      public TransactionPaymentHandler(IStore aStore) : base(aStore) { }
+
+      public override Task<Unit> Handle
+      (
+        TransactionPaymentFullAction aTransactionPaymentFullAction,
+        CancellationToken aCancellationToken
+      )
+      {
+        
+        TransactionState._TransactionsToPay = new List<TransactionDto>();
+        TransactionState._ListOfTransactions = new List<TransactionDto>();
+        return Unit.Task;
+      }
+    }
+  }
+}
